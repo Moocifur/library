167cc8f6531518b255a72888a8e150a052ccf525
David <davidkttran@gmail.com>
2024-06-04 18:35:10 -0700

    added css and html is good

---
+++ script.js
@@ -0,0 +1,29 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+    let newBook = new Book(title, author, pages, read);
+
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function() {
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.style.display = "block";
+})
+
+//shortcut. when add book is clicked
+document.querySelector("#new-book-form").addEventListener("submit", function () {
+    event.preventDefault()
+    addBookToLibrary()
+})
